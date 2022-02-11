c938d6b2afd73dd8e9e2660a96155f41fa40cdf1
Guillaume Roux <rouxguillaume8@gmail.com>
2022-02-11 14:23:25 +0100

    updated with first example

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# flutter-tips
-Some tips and tricks in Flutter &amp; Dart
+# Flutter Tips
+
+Some tips and tricks in Flutter & Dart. 
+
+**Inspired by [vandadnp's](https://github.com/vandadnp) repository [flutter-tips-and-tricks](https://github.com/vandadnp/flutter-tips-and-tricks)**
+
+# Table of Contents
+
+* [JSON decoding](#json-decoding)
+
+# JSON decoding
+
+[Source Code + Tests](source/json_try_decode.dart)
+
+![](images/json_try_decode.jpg)
---
+++ source/json_try_decode.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+typedef JsonDecodeReviver = Object? Function(Object? key, Object? value);
+
+Object? jsonTryDecode(String source, {JsonDecodeReviver? reviver}) {
+  try {
+    return jsonDecode(source, reviver: reviver);
+  } catch (e) {
+    return null;
+  }
+}
+
+// Test it
+test("should return null if invalid source", () {
+  expect(jsonTryDecode(""), isNull);
+});
+
+test("should return a valid JSON if source is correct", () {
+  expect(jsonTryDecode('{"a":1}'), {"a": 1});
+});
